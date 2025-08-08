7a8d46857416789ce46f60e8ce803a4d8a1eff02
Vincent Ostrowski <vincentostrovski@gmail.com>
2025-08-08 11:22:24 -0400

    refactor: remove CardDrawer and SourcesTab components

--- client/src/components/cards_page/card_editor/index.js
+++ client/src/components/cards_page/card_editor/index.js
@@ -1,5 +1,4 @@
 export { default as CardEditModal } from './CardEditModal';
 export { default as CardTab } from './CardTab';
-export { default as SourcesTab } from './SourcesTab';
 export { default as ContentTab } from './ContentTab';
 export { default as QuizitTab } from './QuizitTab'; 
